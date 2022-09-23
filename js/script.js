682f33308a3278dc2079c955a9bf667235c3742d
Juanse <jsab62tol@gmail.com>
2022-09-23 15:50:16 -0500

    First commit of the project

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// ! Script de JavaScript que contendrá la lógica de la aplicación de la calculadora
+
+/* !Para ver que funcione efectivamente el script y esté asociado*/
+document.write("Hola mundo");
